814c80c692a66f3b0c39c6fb5767da60b64f6055
Dmitrii <69720350+demonoved@users.noreply.github.com>
2023-12-29 18:29:13 +0500

    Add files via upload

---
+++ константы.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.20 and less than 0.9.0
+pragma solidity ^0.8.23;
+contract constants{
+    //соглашение о кодирование константы в верхнем регистре
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
